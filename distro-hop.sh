adce52bd078e1db2ba5ed4f1ed05a139198d2623
cytan1981 <23294248+cytan1981@users.noreply.github.com>
2025-10-06 23:01:15 +0800

    Update remove more conflict packages
    Tested on full installation

--- distro-hop.sh
+++ distro-hop.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env -S bash
 
 if [ $# -lt 1 ];then
-    echo "$0 { upgrade_version } { --use-distro-repos }"
+    echo "Usage: `basename $0` { upgrade_version } { --use-distro-repos } { --use-local-repo }"
+    echo "Description:"
+    echo -ne "\t--use-distro-repos : install {DISTO}-repos package from DVD before upgarading\n"
+    echo -ne "\t--use-local-repo : requires a prepared /etc/yum.repos.d/local.repo\n"
+    echo -ne "\t--use-local-repo : will invalid --use-distro-repos\n"
     exit 1
 fi
 
@@ -12,31 +16,35 @@ fi
 # For example: AlmaLinux-10.0-x86_64-dvd.iso
 
 DST_OS_VERSION=$1
+USE_LOCAL_REPO=0
 USE_DISTRO_REPOS=0
 shift
 
 while [ $# -gt 0 ];do
-    case $1 in 
+    case $1 in
+        --use-local-repo)
+            USE_LOCAL_REPO=1
+            shift
+            continue
+            ;;
         --use-distro-repos)
-            # Some packages moved to CRB repo on el10
             USE_DISTRO_REPOS=1
             shift
-            continue;
+            continue
             ;;
-        --mirror)
-            if [ $# -ge 2 ];then
-                MIRROR_URL=$2
-                echo "Mirror URL=$MIRROR_URL"
-                echo "Note: using mirror will ignore --use-distro-repos"
-                shift 2
-                break
-            else
-                echo "Error: Mirror URL not specified!"
-                exit 1
-            fi            
+        *)
+            echo "Unknown option: $1"
+            exit 1
             ;;
     esac
+    shift
 done
+
+# USE_DISTRO_REPO conflicts with USE_LOCAL_REPO
+[ $USE_LOCAL_REPO -eq 1 ] && USE_DISTRO_REPOS=0
+
+# Prevent local.repo being saved to backup directory
+[ $USE_LOCAL_REPO -eq 1 ] && OTHER_REPO_FILE="|local.repo" || OTHER_REPO_FILE=""
 
 MIN_OS_VERSION=8.4
 ARCH=$(uname -m)
@@ -72,14 +80,9 @@ function sigint_func()
 
 check_os_version()
 {  
-    local os=$(echo $OS_NAME | sed -e 's/"//g')
-    local os_ver=$(echo $OS_VERSION | sed -e 's/"//g')
-    echo "Check OS: Found ${os}-${os_ver}"
-
-    if [ "$os" != "${OS_NAME}" ];then
-        echo "Error: Unknow OS $os"
-        return 1
-    fi
+    local os=$OS_NAME
+    local os_ver=$OS_VERSION
+    echo "Check OS: Found ${os}-${os_ver}" 
 
     local os_major_ver=$(echo $os_ver | cut -d '.' -f 1)
     local os_minor_ver=$(echo $os_ver | cut -d '.' -f 2)
@@ -90,28 +93,25 @@ check_os_version()
     local dst_major_ver=$(echo $DST_OS_VERSION | cut -d '.' -f 1)
     local dst_minor_ver=$(echo $DST_OS_VERSION | cut -d '.' -f 2)
 
-    if [ $os_major_ver -eq $minimal_major_ver ];then
-        if [ $os_minor_ver -lt $minimal_minor_ver ];then
-            echo "Error: ${os}-${os_ver} older than minimal version ${OS_NAME}-${MINIMAL_OS_VERSION} !"
-            return 2
-        elif [ $os_minor_ver -ge $minimal_minor_ver ];then
-            echo "Check OS result: OK"
-            return 0
-        fi
+    if [ $os_major_ver -eq $dst_major_ver -a $os_minor_ver -ge $dst_minor_ver ];then
+        echo "Error: $os_ver >= $DST_OS_VERSION , no need to upgrade"
+        return 1
+    elif [ $os_major_ver -eq $dst_major_ver -a $os_minor_ver -lt $dst_minor_ver ];then
+        echo "Error: System minor version is older than expected, update needed"
+        return 1
     fi
 
-    if [ $os_major_ver -eq $dst_major_ver ];then
-        if [ $os_minor_ver -ge $dst_minor_ver ];then
-            echo "Error: $os_ver >= $DST_OS_VERSION , no need to upgrade"
-            return 3
-        fi
-    elif [ $os_major_ver -lt $dst_major_ver ];then
+    if [ $os_major_ver -eq $minimal_major_ver -a $os_minor_ver -lt $minimal_minor_ver ];then
+        echo "Error: Minimal version $MIN_OS_VERSION, found $OS_VERSION"
+        return 1    
+    fi
+
+    if [ $os_major_ver -eq $minimal_major_ver \
+            -a $os_minor_ver -ge $minimal_minor_ver \
+            -o $os_major_ver -lt $dst_major_ver ];then
         echo "Check OS result: OK"
         return 0
     fi
-
-    echo "Error: Minimal os version requires $MIN_OS_VERSION"
-    return 1
 }
 
 function prtlog()
@@ -129,18 +129,19 @@ function prtlog()
 function make_repo_files()
 {
     local logfile=$LOG_DIR/pre_upgrade.log
-    
+    local repo_suffix="-${DST_OS_VERSION}-DVD"
+
     prtlog -f $logfile "Generate $DST_OS_REPO_FILE"
     cd /etc/yum.repos.d
     cat > $DST_OS_REPO_FILE <<-EOF
-[BaseOS]
-name=BaseOS
+[BaseOS$repo_suffix]
+name=BaseOS$repo_suffix
 baseurl=file:///mnt/${DST_OS}/BaseOS
 gpgcheck=0
 enable=1
 
-[AppStream]
-name=AppStream
+[AppStream$repo_suffix]
+name=AppStream$repo_suffix
 baseurl=file:///mnt/${DST_OS}/AppStream
 gpgcheck=0
 enable=1
@@ -194,7 +195,7 @@ function disable_old_repos()
         rpm -q $repos_pkg 2>&1>/dev/null && rpm -e --nodeps  2>&1 >> $logfile
         saved_list=$(/bin/ls -A *.repo 2>/dev/null)
     else
-        saved_list=$(/bin/ls -A *.repo 2>/dev/null | $EGREP -v "$DST_OS_REPO_FILE")
+        saved_list=$(/bin/ls -A *.repo 2>/dev/null | $EGREP -v "$DST_OS_REPO_FILE${OTHER_REPO_FILE}")
     fi
 
     [ -n "$saved_list" ] && /bin/mv -f $saved_list $saved_dir
@@ -215,51 +216,51 @@ function upgrade()
     fi
 
     prtlog "Remove conflict packages"
-    
+    for pkg in ${OS_NAME}-logos iptables-ebtables alsa-sof-firmware crda;do
+        rpm -q $pkg 2>&1 > /dev/null && rpm -e --nodeps $pkg 2>&1 >> $logfile
+    done
+    # rpm -q crda 2>&1 > /dev/nulll && dnf remove -y crda 2>&1 >> $logfile
     [ "${DST_OS_CLASS}" = "el9" ] && {
         prtlog "Remove initscripts"
         yum remove -y initscripts 2>&1 >> $logfile
     }
 
-    for pkg in ${OS_NAME}-logos iptables-ebtables;do
-        rpm -q $pkg 2>&1 > /dev/null && rpm -e --nodeps $pkg 2>&1 >> $logfile
-    done
+    if [ $USE_DISTRO_REPOS -eq 1 ];then
+        cd /etc/yum.repos.d
+        local saved_dir=saved_${DST_OS_CLASS}
+        mkdir -p $saved_dir
+        local saved_list=$(/bin/ls -A *.repo 2>/dev/null)
+        [ -n "${saved_list}" ] && /bin/mv -f $saved_list $saved_dir
 
-    if [ "${SRC_OS_CLASS}" = "el8" -a "${DST_OS_CLASS}" = "el10" ];then
-        rpm -q crda 2>&1 > /dev/nulll && dnf remove -y crda 2>&1 >> $logfile
-        
+        find $DST_OS_ISO_MP \
+            -regextype egrep -regex ".*-(release|gpg-keys|repos)-.*" \
+        | while read pkg;do 
+            rpm -ivh --nodeps --force $pkg 2>&1 >> $logfile
+        done
+
+        cd /etc/yum.repos.d
+        for repo_file in *.repo;do             
+            [ -e ${repo_file}.orig ] || sed -i.orig -e "s/\$releasever/${DST_OS_VERSION}/g" $repo_file
+        done
+
         # Some packages like virt-manager are moved CRB repo on el10
-        if [ $USE_DISTRO_REPOS -eq 1 ];then
-            cd /etc/yum.repos.d
-            local saved_dir=saved_${DST_OS_CLASS}
-            mkdir -p $saved_dir
-            local saved_list=$(/bin/ls -A *.repo 2>/dev/null)
-            [ -n "${saved_list}" ] && /bin/mv -f $saved_list $saved_dir
-
-            find $DST_OS_ISO_MP \
-                -name "${OS_NAME}-repos-*" -o \
-                -name "${OS_NAME}-gpg-keys-*" -o \
-                -name "${OS_NAME}-release-*" \
-            | while read pkg;do 
-                rpm -ivh --nodeps --force $pkg 2>&1 >> $logfile
-            done
-
-            cd /etc/yum.repos.d
-            for repo_file in *.repo;do             
-                [ -e ${repo_file}.orig ] || sed -i.orig -e "s/\$releasever/${DST_OS_VERSION}/g" $repo_file
-            done
-
+        if [ "${DST_OS_CLASS}" = "el10" ];then
             prtlog "Enable CRB repo for ${DST_OS_CLASS}"
-            (yum config-manager --enable crb
-            yum clean all; yum makecache) >> $logfile
+            yum config-manager --enable crb 2>&1 >> $logfile
         fi
+
+        (yum clean all; yum makecache) >> $logfile
     fi
 
     prtlog ">>> Upgrade ${OS_NAME}-${OS_VERSION} to ${OS_NAME}-${DST_OS_VERSION} by dnf distro-sync"
-    (dnf -y --releasever=$DST_OS_VERSION --allowerasing --setopt=deltarpm=false --nobest distro-sync \
+    (dnf -y --releasever=$DST_OS_VERSION \
+        --allowerasing \
+        --setopt=deltarpm=false \
+        --nobest distro-sync \
         && update-crypto-policies --set LEGACY \
-        && rpm --rebuilddb) 2>&1 >> $LOG_FILE \
-        && touch $LOG_DIR/upgrade.done
+        && rpm --rebuilddb
+    ) 2>&1 >> $LOG_FILE \
+    && touch $LOG_DIR/upgrade.done
 }
 
 function post_upgrade()
@@ -309,7 +310,7 @@ function post_upgrade()
         else
             prtlog -f $logfile "Saved repo files of ${OS_NAME}-repos to /etc/yum.repos.d/${saved_dir}"
             prtlog -f $logfile "Warning: Some old packages like virt-manager will be removed when using ISO repo only"
-            saved_list=$(/bin/ls -A | $EGREP -v "${DST_OS_REPO_FILE}|$saved_dir|saved")
+            saved_list=$(/bin/ls -A | $EGREP -v "${DST_OS_REPO_FILE}|$saved_dir|saved${OTHER_REPO_FILE}")
             [ -n "$saved_list" ] && /bin/mv -f $saved_list $saved_dir
         fi
     fi
@@ -328,7 +329,7 @@ function post_upgrade()
         | $EGREP -v ".${current_os_class}" > $pkgs_list
         /bin/cp -af $pkgs_list ${pkgs_list}.orig
     fi
-    
+
     prtlog -f $logfile "Generate thirdparty packages list"
     $EGREP -v "$EXCLUDE_VENDORS" ${pkgs_list}.orig > $other_pkgs
 
@@ -362,8 +363,9 @@ function post_upgrade()
         rpm -e --nodeps $(awk -F '@' '{ printf("%s-%s-%s.%s\n", $1, $2,$3,$4); }' $pkgs_list)
     fi
     systemctl daemon-reload
-    cd /usr/bin
-    /bin/ln -sf $(/bin/ls -A python[0-9].*) python3
+
+    prtlog -f $logfile "Create symlink /usr/bin/python3 for yum/dnf"
+    cd /usr/bin; /bin/ln -sf $(/bin/ls -A python[0-9].*) python3
 
     prtlog -f $logfile "Make yum cache after removing old packages"
     (yum clean all
@@ -387,7 +389,7 @@ function post_upgrade()
         dnf install --skip-broken -y $(cat $install_list) 2>&1 >> $logfile
     fi
 
-    prtlog -f $logfile "Install back packages after distro-sync"
+    prtlog -f $logfile "Install back packages after upgraded to ${DST_OS_CLASS}"
     case $DST_OS_CLASS in
         el9)
             yum install -y audit initscripts 2>&1 >> $logfile
@@ -397,20 +399,23 @@ function post_upgrade()
             (find $DST_OS_ISO_MP -name 'javapackages-filesystem*' -exec dnf localinstall -y {} \;) 2>&1 >> $logfile
             ;;
     esac
+    
+    # dbus-daemon is disabled after upgraded to el10
+    systemctl is-enabled dbus-daemon 2>&1 > /dev/null || {
+        prtlog -f $logfile "Enable dbus-daemon.service"
+        systemctl enable -f dbus-daemon 2>&1 >> $logfile
+    }
 
-    # dbus-daemon is disabled after distro-sync
-    prtlog -f $logfile "Enable dbus-daemon.service after distro-sync"
-    systemctl enable dbus-daemon 2>&1 >> $logfile
-
-    # Migrate NetworkManager connections
-    prtlog -f $logfile "Migrate NetworkManager connections for ${DST_OS_CLASS}"
-    (systemctl restart dbus-daemon NetworkManager
-    nmcli conn migrate
-    cd /etc/NetworkManager/system-connections        
-    for netdev in $(nmcli c s | sed -n -e '2,$p' | cut -d ' ' -f 1);do
-        nmcli conn modify $netdev connection.autoconnect yes
-    done
-    ) 2>&1 >> $logfile
+    if [ "${DST_OS_CLASS}" = "el10" ];then
+        prtlog -f $logfile "Migrate NetworkManager connections for ${DST_OS_CLASS}"
+        (systemctl restart dbus-daemon NetworkManager
+        nmcli conn migrate
+        cd /etc/NetworkManager/system-connections        
+        for netdev in $(nmcli c s | sed -n -e '2,$p' | cut -d ' ' -f 1);do
+            nmcli conn modify $netdev connection.autoconnect yes
+        done
+        ) 2>&1 >> $logfile
+    fi
 
     prtlog -f $logfile "Update grub boot title for $DST_OS"
     os_title=$($EGREP -o '[0-9.]{2,}.*' /etc/${OS_NAME}-release)
@@ -440,13 +445,8 @@ trap sigint_func SIGINT
     exit $?
 }
 
-check_os_version
-retval=$?
-if [ $retval -eq 0 ];then
-    mount_repos
-    make_repo_files
-    disable_old_repos
-    upgrade && post_upgrade
-else
-    exit 1
-fi
+check_os_version || exit 1
+mount_repos
+make_repo_files
+disable_old_repos
+upgrade && post_upgrade
